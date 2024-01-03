a0a5ea3edde2af405f495864cd2cf889333a8c8d
Amir Jaraysa <amirjaraysa@gmail.com>
2024-01-02 21:16:02 -0500

    created authentification page component alongside sign in form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import SignIn from './routes/sign-in/sign-in.component';
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Authentification from "./routes/authentification/authentification.component";
 
 const Shop = () => {
-  return (
-    <h1>I am the shop page</h1>
-  )
-}
+  return <h1>I am the shop page</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Navigation />}>
+      <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
-        <Route path='sign-in' element={<SignIn />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="auth" element={<Authentification />} />
       </Route>
     </Routes>
-  )
-}
+  );
+};
 
 export default App;
