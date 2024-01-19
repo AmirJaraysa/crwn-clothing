fd15ba6dce8881c90c5bc297fc5bac7394830cd3
Amir Jaraysa <amirjaraysa@gmail.com>
2024-01-18 23:14:18 -0500

    implemented payment method with stripe

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import "./index.scss";
-
+import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { store, persistor } from "./store/store";
+import "./index.scss";
 
 const rootElement = document.getElementById("root");
 
@@ -16,7 +17,9 @@ createRoot(rootElement).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
