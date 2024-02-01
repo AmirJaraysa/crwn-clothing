5247de1bf904a94f2fbabf05b7f21e8d1d5f5ee9
JaraysaAmir <jaraysaa@kean.edu>
2024-02-01 16:38:35 -0500

    converted project  over to typescript

---
+++ src/store/user/user.reducer.ts
@@ -0,0 +1,47 @@
+import { AnyAction } from "redux-saga";
+import { USER_ACTION_TYPES } from "./user.types";
+import {
+  signInFailed,
+  signUpFailed,
+  signOutFailed,
+  signOutSuccess,
+  signInSuccess,
+} from "./user.action";
+import { UserData } from "../../utils/firebase/firebase.utils";
+import { act } from "@testing-library/react";
+
+export type UserState = {
+  readonly currentUser: UserData | null;
+  readonly isLoading: boolean;
+  readonly error: Error | null;
+};
+
+export const INITIAL_STATE: UserState = {
+  currentUser: null,
+  isLoading: false,
+  error: null,
+};
+
+export const userReducer = (state = INITIAL_STATE, action: AnyAction) => {
+  if (signInSuccess.match(action)) {
+    return {
+      ...state,
+      currentUser: action.payload,
+    };
+  }
+  if (signOutSuccess.match(action)) {
+    return {
+      ...state,
+      currentUser: null,
+    };
+  }
+
+  if (
+    signInFailed.match(action) ||
+    signUpFailed.match(action) ||
+    signOutFailed.match(action)
+  ) {
+    return { ...state, error: action.payload };
+  }
+  return state;
+};
